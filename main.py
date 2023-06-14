ee2febba464aac2d0f1b9213287fd0d2c67af8b1
Lim Wei Jen <limweijen96@gmail.com>
2023-06-14 17:58:00 +0800

    feat: New text summarizer

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from newsdataapi import NewsDataApiClient
 from transformers import pipeline
 import openpyxl
 import datetime
-import os
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
 
 # API key authorization
-api = NewsDataApiClient(os.environ.get('API_KEY'))
+api = NewsDataApiClient(apikey="pub_20239668dc2992683ebca80bca8b4951aab1e")
 
 # get the news
 response = api.news_api(language = "en")
@@ -19,18 +21,51 @@ def summarize_content(content):
     """
     This function takes a string of text as input and returns a summary of the content.
     """
-    try:
-        # Initializing the summarization pipeline
-        summarizer = pipeline("summarization")
+    stopWords = set(stopwords.words("english"))
+    words = word_tokenize(content)
+    freqTable = dict()
+    for word in words:
+        word = word.lower()
+        if word in stopWords:
+            continue
+        if word in freqTable:
+            freqTable[word] += 1
+        else:
+            freqTable[word] = 1
+    sentences = sent_tokenize(content)
+    sentenceValue = dict()
 
-        # Generating the summary
-        summary = summarizer(content, max_length=100, min_length=30, do_sample=False)
+    for sentence in sentences:
+        for word, freq in freqTable.items():
+            if word in sentence.lower():
+                if sentence in sentenceValue:
+                    sentenceValue[sentence] += freq
+                else:
+                    sentenceValue[sentence] = freq
+    sumValues = 0
+    for sentence in sentenceValue:
+        sumValues += sentenceValue[sentence]
 
-        # Returning the summary as a string
-        return summary[0]['summary_text']
+    average = int(sumValues / len(sentenceValue))
+
+    summary = ''
+    for sentence in sentences:
+        if (sentence in sentenceValue) and (sentenceValue[sentence] > (1.2 * average)):
+            summary += " " + sentence
+    return summary
+
+    # try:
+    #     # Initializing the summarization pipeline
+    #     summarizer = pipeline("summarization", model="gpt2", tokenizer="gpt2", framework="tf")
+
+    #     # Generating the summary
+    #     summary = summarizer(content, max_length=100, min_length=30, do_sample=False)
+
+    #     # Returning the summary as a string
+    #     return summary[0]['summary_text']
     
-    except IndexError:
-        return False
+    # except IndexError:
+    #     return False
 
 # write the news into the worksheet
 for news in lst:
